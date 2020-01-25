5911d305aa89ba13b9390770cc3f0bd62c18a47d
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-01-25 19:10:39 -0300

    Classe Abstrata e Polimorfismo
    +Criado uma classe abstrata com seus Estados e Comportamentos.
    
    +Criado uma classe concreda da classe abstrata com polimorfismo.

---
+++ oop/Principios-fundamentais/AbstractClass/CafeteiraEspressa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop.Principiosfundamentais.AbstractClass
+{
+    public class CafeteiraEspressa : Eletrodomestico
+    {
+        public CafeteiraEspressa(string nome, string voltagem) : base(nome, voltagem)
+        {
+        }
+
+        public CafeteiraEspressa() 
+            : base("Padrao", "220w") { }
+
+        private static void AquecerAgua() { }
+
+        private static void MoerGraos() { }
+
+        public void PrepararCafe()
+        {
+            AquecerAgua();
+            MoerGraos();
+        }
+
+        public override void Testar()
+        {
+            //Testar cafeteira
+        }
+
+
+        public override void Desligar()
+        {
+            //Verificar recipiente de água
+        }
+
+        public override void Ligar()
+        {
+            //Resfriar o aquecedor
+        }
+    }
+}
---
+++ oop/Principios-fundamentais/AbstractClass/Eletrodomestico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop.Principiosfundamentais.AbstractClass
+{
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly string _voltagem;
+
+        public Eletrodomestico(string nome, string voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+
+        public abstract void Desligar();
+
+        public virtual void Testar() { }
+
+    }
+}
